f2e4d86bc78dcfda9fe6d22b258eb9c267c30f38
edlectrico <edlectrico@gmail.com>
2014-11-27 10:38:33 +0100

    New methods and example

--- 4_system_architecture/api_adaptation.java
+++ 4_system_architecture/api_adaptation.java
@@ -1,26 +1,15 @@
-private Button button1, button2;
-private EditText edit1, edit2;
-private TextView textView1, textView2;
-private List<View> buttons, editTexts, textViews;
+private static final NAMESPACE = "http://www.morelab.deusto.es/ontologies/adaptui.owl#"
 
 @Override
 protected void onCreate(Bundle savedInstanceState) {
   super.onCreate(savedInstanceState);
   setContentView(R.layout.activity);
 
-  button1 = (Button) findViewById(R.id.button1);
-  //button2, button3...
-  buttons.add(button1);
-  //...
+  Button button = (Button) findViewById(R.id.button1);
+  
+  AdaptUI adaptUI = new AdaptUI();
+  
+  List<String> buttons = adaptUI.getIndividualsOfClass(NAMESPACE, "Button");
 
-  //Initializing adaptui
-  AdaptuiAdapter adapter = new AdaptuiAdapter(getApplicationContext());
-
-  //Calling different needed methods
-  drawViews(adapter)
-}
-
-private void drawViews(AdaptuiAdapter adapter){
-  adapter.adapt(buttons);
-  //The same for other ui components
+  button.setBackgroundColor(
 }
