e43649191baaa54a6cebe442d7b7ddaa5e92f784
edlectrico <edlectrico@gmail.com>
2014-11-28 11:16:20 +0100

    Fixing tabs

--- 4_system_architecture/android_activity.java
+++ 4_system_architecture/android_activity.java
@@ -1,15 +1,14 @@
-  public class ExampleActivity extends Activity {
+public class ExampleActivity extends Activity {
+  private Button button;
     
-    private Button button;
-    
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        // Here we can initialize components for this activities,
-        // such as buttons, text views, etc.
-        
-        button = (Button) findViewById(R.id.button_example);
+    // Here we can initialize components for this activities,
+    // such as buttons, text views, etc.
+    button = (Button) findViewById(R.id.button_example);
+    // ...
     }
 }
--- 4_system_architecture/shared_preferences_complex.java
+++ 4_system_architecture/shared_preferences_complex.java
@@ -1,31 +1,30 @@
 public class CustomImplementation implements Parcelable {
+  private int intAttribute;
+  private float floatAttribute;
 
-private int intAttribute;
-private float floatAttribute;
+  public UserMinimumPreferences(int intAttr, float floatAttr) {
+    super();
+    this.intAttribute = intAttr;
+    this.floatAttribute = floatAttr;
+  }
 
-public UserMinimumPreferences(int intAttr, float floatAttr) {
-  super();
-  this.intAttribute = intAttr;
-  this.floatAttribute = floatAttr;
-}
+  public CustomImplementation(Parcel in) { 
+    readFromParcel(in); 
+  }
 
-public CustomImplementation(Parcel in) { 
-  readFromParcel(in); 
-}
+  public static Parcelable.Creator<CustomImplementation> getCreator() {
+    return CREATOR;
+  }
 
-public static Parcelable.Creator<CustomImplementation> getCreator() {
-  return CREATOR;
-}
+  // Writing in the Parcelable object
+  @Override
+  public void writeToParcel(Parcel dest, int flags) {
+    dest.writeInt(intAttribute);
+    dest.writeFloat(floatAttribute);
+  }
 
-//Writing in the Parcelable object
-@Override
-public void writeToParcel(Parcel dest, int flags) {
-  dest.writeInt(intAttribute);
-  dest.writeFloat(floatAttribute);
-}
-
-//Reading from the Parcelable object
-private void readFromParcel(Parcel in) {   
-  intAttribute = in.readInt();
-  floatAttribute = in.readFloat();
-}
+  // Reading from the Parcelable object
+  private void readFromParcel(Parcel in) {   
+    intAttribute = in.readInt();
+    floatAttribute = in.readFloat();
+  }
