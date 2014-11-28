5d4cc1d3db9b7b96c3ee75a5d2d63efa54bb3ba2
edlectrico <edlectrico@gmail.com>
2014-11-28 11:19:02 +0100

    Fixing code length

--- 4_system_architecture/abstract_activity.java
+++ 4_system_architecture/abstract_activity.java
@@ -1,7 +1,6 @@
 public abstract class AbstractActivity extends 
 	  Activity implements View.OnClickListener, 
 	  View.OnLongClickListener, TextToSpeech.OnInitListener {
-
   //The ontology individuals
   private List<String> buttons, edits, textViews, audios, 
   displays, noises, lights, backgrounds, devices, contexts;
@@ -12,8 +11,7 @@ public abstract class AbstractActivity extends
   public void initOntology() {
     buttons = ontManager.getIndividualOfClass("Button");
     edits = ontManager.getIndividualOfClass("EditText");
-    //And so forth with the rest of declared individuals		
-     
+    // And so forth with the rest of declared individuals		
     ontology_initialized = true;
   }
 
@@ -29,7 +27,7 @@ public abstract class AbstractActivity extends
     views.add(getTextViews().get(0));
     views.add(getBackgrounds().get(0));
     
-    //For each view every value is removed
+    // For each view every value is removed
     for (int i = 0; i < views.size(); i++){
       ontManager.removeIndividualMembership(
 			views.get(i), "viewHasColor");
@@ -43,7 +41,7 @@ public abstract class AbstractActivity extends
 			views.get(i), "viewHasHeight");
     }
     
-    //Finally the membership is also removed
+    // Finally, the membership is also removed
     ontManager.removeIndividualMembership(
 			getAudios().get(0), "audioHasVolume");
     ontManager.removeIndividualMembership(
@@ -53,6 +51,5 @@ public abstract class AbstractActivity extends
     //...and so forth
   }
 
-  //The rest of methos of the class
   //...
 }
