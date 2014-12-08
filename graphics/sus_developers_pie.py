dae68bf41e5ec7f4dc775023ca8558db140fcebf
edlectrico <edlectrico@gmail.com>
2014-12-08 12:06:58 +0100

    SUS developers pie graphic updated to 30 users

--- graphics/sus_developers_pie.py
+++ graphics/sus_developers_pie.py
@@ -6,8 +6,8 @@ figure(1, figsize=(6,6))
 ax = axes([0.1, 0.1, 0.8, 0.8])
 
 # The slices will be ordered and plotted counter-clockwise.
-labels = ['x<50', '50<=x<70', 'x>=70']
-fracs = [0, 18.18181818, 72.72727273]
+labels = ['SUS punctuation under 50 points', 'SUS punctuation between 50 and 70 points', 'SUS punctuation over or equal 70 points']
+fracs = [0, 16.66666667, 75]
 explode=(0.05, 0.05, 0.05)
 colors = ['yellowgreen', 'gold', 'lightskyblue']
 '''
@@ -22,7 +22,7 @@ patches = pie(fracs, explode=explode, autopct='%1.1f%%', colors=colors, startang
 
 #plt.pie(sizes, colors=colors, shadow=True)
 plt.axis('equal')
-plt.legend(labels, loc=(-0.05, 0.05), shadow=True)
+plt.legend(labels, loc=(-0.1, -0.1), shadow=False)
 #plt.savefig(name)
 
 plt.savefig('sus_developers.pdf')
