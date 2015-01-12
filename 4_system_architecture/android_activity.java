f875788610cc379495bc77f75b08d654d638d18a
edlectrico <edlectrico@gmail.com>
2015-01-12 12:51:22 +0100

    Reviewed

--- 4_system_architecture/android_activity.java
+++ 4_system_architecture/android_activity.java
@@ -6,8 +6,8 @@ public class ExampleActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    // Here we can initialize components for this activities,
-    // such as buttons, text views, etc.
+    // Here the developer can initialize components for 
+    // this activity, such as buttons, text views, etc.
     button = (Button) findViewById(R.id.button_example);
     // ...
     }
